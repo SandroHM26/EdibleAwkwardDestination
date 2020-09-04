6e4127ff1b9b3435622fef4ae9616e8a26f3ca59
SandroHM26 <a20171364@pucp.edu.pe>
2020-09-04 23:09:22 +0000

    ya acabe gaaaaa

--- main.c
+++ main.c
@@ -2,15 +2,6 @@
 
 int main(void) {
   printf("Hola mundo soy Sandro\n");
-  int suma = 0;
-  while(1){
-    if(suma == 10){
-      printf("La suma llegó a 10\n");
-      break;
-    }
-    else{
-      suma++;
-    }
-  }
+
   return 0;
 }
