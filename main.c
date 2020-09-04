bf129b26a35ed62969d750f20e3c6bb63dc05c2c
SandroHM26 <a20171364@pucp.edu.pe>
2020-09-04 16:32:56 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hola mundo\n");
+  int suma = 0;
+  while(1){
+    if(suma == 10){
+      printf("La suma llegó a 10\n");
+      break;
+    }
+    else{
+      suma++;
+    }
+  }
+  return 0;
+}
